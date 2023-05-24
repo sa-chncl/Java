b507af7ef09c780c8aaf28afaa933fecd1cd6874
“[Chanchal <[chinkishrivastava222@gmail.com]>
2023-05-24 02:47:14 -0500

    Multiple client server Program

---
+++ Client/src/test/java/com/Chanchal/demo/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Chanchal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
